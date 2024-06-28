d608c7f77567c115c5e8243f178ce0c701d7ad7f
Fallen-Angel61 <debarshigupta47@gmail.com>
2024-06-28 11:47:33 +0530

    Initial Project Setup

---
+++ src/main/java/com/debarshi/learnscape/LearnscapeApplication.java
@@ -0,0 +1,13 @@
+package com.debarshi.learnscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnscapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnscapeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/debarshi/learnscape/LearnscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debarshi.learnscape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnscapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
